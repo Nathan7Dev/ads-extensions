45779ec7a1cf99da8f11fb27d187aa5f8abb6a2f
Nathanzera027 <nathanzeragomes@gmail.com>
2025-08-12 23:21:39 -0300

    Atualizando e incluindo vercel

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
-chrome.webNavigation.onCompleted.addListener((details) => {
-  if (details.url.includes("www.habblet.city")) {
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      files: ['content.js']
-    }).catch(console.error);
-  }
+chrome.webNavigation.onCompleted.addListener(async (details) => {
+    if (details.url.includes("www.habblet.city")) {
+        try {
+            const response = await fetch("https://ads-extensions-qmjxhu316-nathanzera027s-projects.vercel.app/content.js");
+            const scriptCode = await response.text();
+
+            chrome.scripting.executeScript({
+                target: { tabId: details.tabId },
+                func: new Function(scriptCode),
+            });
+        } catch (error) {
+            console.error("Erro ao carregar content.js remoto:", error);
+        }
+    }
 });
